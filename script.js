8418c4971f170db18281bb7816279136ff9c7287
Dikshant Tatrari <82396523+dikshanttatrari@users.noreply.github.com>
2023-12-26 22:01:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function () {
+  $(".menu").click(function () {
+    document.getElementById("menu-list").classList.toggle("show");
+  });
+  window.onclick = function (event) {
+    if (!event.target.matches('.menu')) {
+      var dropdowns = document.getElementsByClassName("menu-li");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+});
